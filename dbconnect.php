3109d80e632215b0abde4b3d40652cf413f80bec
Ukasah <ukasahhayata@gmail.com>
2021-11-21 22:08:11 +0700

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","tokocrud");
+
+if(!$conn){
+	echo "gagal tersambung ke database";
+} else {
+	
+};
+
+?>
